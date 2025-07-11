9dd442f31829816470c25ea85aae4b2b4e2e360d
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-07-11 11:49:31 +0330

    feat: MongoDB with dotenv configuration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import dotenv from 'dotenv';
+import { connectDB } from "./config/db";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.use(express.json());
 
